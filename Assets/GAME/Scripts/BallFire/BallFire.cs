1e0a72c63323868437b89446135bf2701023d64c
Ring <85309246+Pinochio195@users.noreply.github.com>
2023-06-14 18:40:43 +0700

    update

--- Assets/GAME/Scripts/BallFire/BallFire.cs
+++ Assets/GAME/Scripts/BallFire/BallFire.cs
@@ -42,7 +42,6 @@ public class BallFire : MonoBehaviour, ICollidable
         ContactPoint2D contact = collision.GetContact(0);
         Vector2 contactPoint = contact.point;
         Vector2 contactNormal = contact.normal;
-        LeanPool.Despawn(PlayerController.Instance._playerFire._ballFireGameObject);
 
         #region lấy góc cho Cổng
 
@@ -53,6 +52,92 @@ public class BallFire : MonoBehaviour, ICollidable
         }
 
         #endregion
+
+        LeanPool.Despawn(PlayerController.Instance._playerFire._ballFireGameObject);
+
+        #region Check space để spawn portal
+
+        if (_typeBall == TypeBall.Blue)
+        {
+            var portalRed = PortalManager.Instance._portalSpawn._portalRed;
+            var redCollider = portalRed != null ? portalRed.GetComponent<Collider2D>() : null;
+            var portalBounds = redCollider != null ? redCollider.bounds : default;
+
+            if (angle == 90 || angle == -90)
+            {
+                if (portalRed != null && contactPoint.y < portalRed.transform.position.y)
+                {
+                    if (Mathf.Abs(portalBounds.min.y - wallCollider.bounds.min.y) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    if (Mathf.Abs(portalBounds.max.y - wallCollider.bounds.max.y) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                if (portalRed != null && contactPoint.x < portalRed.transform.position.x)
+                {
+                    if (Mathf.Abs(portalBounds.min.x - wallCollider.bounds.min.x) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+
+                if (Mathf.Abs(portalBounds.max.y - wallCollider.bounds.max.y) < portalBounds.size.y)
+                {
+                    return;
+                }
+            }
+        }
+        else if (_typeBall == TypeBall.Red)
+        {
+            var portalBlue = PortalManager.Instance._portalSpawn._portalBlue;
+            var blueCollider = portalBlue != null ? portalBlue.GetComponent<Collider2D>() : null;
+            var portalBounds = blueCollider != null ? blueCollider.bounds : default;
+
+            if (angle == 90 || angle == -90)
+            {
+                if (portalBlue != null && contactPoint.y < portalBlue.transform.position.y)
+                {
+                    if (Mathf.Abs(portalBounds.min.y - wallCollider.bounds.min.y) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    if (Mathf.Abs(portalBounds.max.y - wallCollider.bounds.max.y) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                if (portalBlue != null && contactPoint.x < portalBlue.transform.position.x)
+                {
+                    if (Mathf.Abs(portalBounds.min.x - wallCollider.bounds.min.x) < portalBounds.size.y)
+                    {
+                        return;
+                    }
+                }
+
+                if (Mathf.Abs(portalBounds.max.y - wallCollider.bounds.max.y) < portalBounds.size.y)
+                {
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
 
         //Spawn cổng
         if (_typeBall == TypeBall.Blue)
@@ -65,42 +150,21 @@ public class BallFire : MonoBehaviour, ICollidable
             PortalManager.Instance._portalSpawn._portalBlue = LeanPool.Spawn(
                 PortalManager.Instance._portalSpawn._prefabsPortalBlue, collision.transform.position,
                 Quaternion.Euler(0, 0, angle));
-            //lấy collider của cổng
+
+            #region Các biến nạp vào hàm
+
             Collider2D portalCollider = PortalManager.Instance._portalSpawn._portalBlue.GetComponent<Collider2D>();
             Vector2 wallCenter = wallCollider.bounds.center;
             Vector2 portalColliderSize = portalCollider.bounds.size;
-            float wallWidth = wallCollider.bounds.size.x;
-            if (angle == 90 || angle == -90)
-            {
-                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+            float wallWidthX = wallCollider.bounds.size.x;
+            float wallWidthY = wallCollider.bounds.size.y;
 
-                float portalWidth = portalColliderSize.x;
-                float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
-                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-                newPosition.y = contactPoint.y;
-                float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
-                    wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
-                newPosition.y = newY;
-                PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;
+            #endregion
 
-                #endregion
-            }
-            else
-            {
-                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
-
-                float wallHeight = wallCollider.bounds.size.y;
-                float portalHeight = portalColliderSize.y;
-                float distanceFromWallCenter = (wallHeight + portalHeight) * 0.5f;
-                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-                newPosition.x = contactPoint.x;
-                float newX = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
-                    wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
-                newPosition.x = newX;
-                PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;
-
-                #endregion
-            }
+            //lấy collider của cổng
+            CheckUpDownPortal(wallCollider, contactPoint, angle, portalColliderSize, wallWidthX , wallWidthY, wallCenter,
+                contactNormal, PortalManager.Instance._portalSpawn._portalBlue,
+                PortalManager.Instance._portalSpawn._portalRed);
         }
         else if (_typeBall == TypeBall.Red)
         {
@@ -112,51 +176,199 @@ public class BallFire : MonoBehaviour, ICollidable
             PortalManager.Instance._portalSpawn._portalRed = LeanPool.Spawn(
                 PortalManager.Instance._portalSpawn._prefabsPortalRed, collision.transform.position,
                 Quaternion.Euler(0, 0, angle));
-            //lấy collider của cổng
+
+            #region Các biến nạp vào hàm
+
             Collider2D portalCollider = PortalManager.Instance._portalSpawn._portalRed.GetComponent<Collider2D>();
             Vector2 wallCenter = wallCollider.bounds.center;
             Vector2 portalColliderSize = portalCollider.bounds.size;
             float wallWidth = wallCollider.bounds.size.x;
-            if (angle == 90 || angle == -90)
-            {
-                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+            float wallWidthY = wallCollider.bounds.size.y;
+            #endregion
 
-                float portalWidth = portalColliderSize.x;
-                float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
-                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-                newPosition.y = contactPoint.y;
-                float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
-                    wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
-                newPosition.y = newY;
-                PortalManager.Instance._portalSpawn._portalRed.transform.position = newPosition;
-
-                #endregion
-            }
-            else
-            {
-                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
-
-                float wallHeight = wallCollider.bounds.size.y;
-                float portalHeight = portalColliderSize.y;
-                float distanceFromWallCenter = (wallHeight + portalHeight) * 0.5f;
-                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-                newPosition.x = contactPoint.x;
-                float newX = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
-                    wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
-                newPosition.x = newX;
-                PortalManager.Instance._portalSpawn._portalRed.transform.position = newPosition;
-
-                #endregion
-            }
+            //lấy collider của cổng
+            CheckUpDownPortal(wallCollider, contactPoint, angle, portalColliderSize, wallWidth , wallWidthY, wallCenter,
+                contactNormal, PortalManager.Instance._portalSpawn._portalRed,
+                PortalManager.Instance._portalSpawn._portalBlue);
         }
     }
 
+    private void CheckUpDownPortal(Collider2D wallCollider, Vector2 contactPoint, float angle,
+        Vector2 portalColliderSize,
+        float wallWidthX , float wallWidthY, Vector2 wallCenter, Vector2 contactNormal, GameObject portalSpawn,
+        GameObject portalCurrent)
+    {
+        if (angle == 90 || angle == -90)
+        {
+            ChangePortal_90_Nega90(wallCollider, angle, portalColliderSize, wallWidthX, wallCenter, contactNormal,
+                contactPoint,
+                portalSpawn,
+                portalCurrent);
+        }
+        else
+        {
+            ChangePortal_0_Nega180(wallCollider, angle, portalColliderSize, wallWidthY, wallCenter, contactNormal,
+                contactPoint,
+                portalSpawn,
+                portalCurrent);
+        }
+    }
+
+    #region Angle = 90 hoặc -90
+
+    private void ChangePortal_90_Nega90(Collider2D wallCollider, float angle, Vector2 portalColliderSize, float wallWidth,
+        Vector2 wallCenter, Vector2 contactNormal, Vector2 contactPoint, GameObject portalSpawn,
+        GameObject portalCurrent)
+    {
+        Spawn_90_Nega90(wallCollider, portalColliderSize, wallCenter, contactNormal, contactPoint, portalSpawn , wallWidth);
+        if (portalCurrent != null)
+        {
+           
+            //đang cùng 1 cột
+            if (Mathf.Abs(PortalManager.Instance._portalSpawn._portalRed.transform.position.x -
+                          PortalManager.Instance._portalSpawn._portalBlue.transform.position.x) < 0.55f)
+            {
+                if (portalCurrent != null)
+                {
+                    float sizePortal = portalCurrent.GetComponent<Collider2D>().bounds.size.y / 2;
+                    float minY = 0;
+                    float maxY = 0;
+                    if (Mathf.Abs(portalSpawn.transform.position.y - portalCurrent.transform.position.y) <
+                        portalCurrent.GetComponent<Collider2D>().bounds.size.y)
+                    {
+                        Debug.Log(123);
+                        if (contactPoint.y > portalCurrent.transform.position.y)
+                        {
+                            minY = portalCurrent.GetComponent<Collider2D>().bounds.max.y + sizePortal;
+                            maxY = wallCollider.bounds.max.y - sizePortal;
+                            Vector2 newPosition1 = (Vector2)portalCurrent.transform.position +
+                                                   new Vector2(0,
+                                                       portalCurrent.GetComponent<Collider2D>().bounds.size.y +
+                                                       .3f);
+                            newPosition1.y = Mathf.Clamp(newPosition1.y, minY, maxY);
+                            portalSpawn.transform.position = newPosition1;
+                        }
+                        else
+                        {
+                            maxY = portalCurrent.GetComponent<Collider2D>().bounds.min.y - sizePortal;
+                            minY = wallCollider.bounds.min.y + sizePortal;
+                            Vector2 newPosition1 = (Vector2)portalCurrent.transform.position -
+                                                   new Vector2(0,
+                                                       portalCurrent.GetComponent<Collider2D>().bounds.size.y +
+                                                       .3f);
+                            Debug.Log(newPosition1.y);
+                            newPosition1.y = Mathf.Clamp(newPosition1.y, minY, maxY);
+                            Debug.Log(newPosition1.y);
+                            portalSpawn.transform.position = newPosition1;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    private void Spawn_90_Nega90(Collider2D wallCollider, Vector2 portalColliderSize, Vector2 wallCenter, Vector2 contactNormal,
+        Vector2 contactPoint, GameObject portalSpawn , float wallWidth)
+    {
+        #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+        float portalWidth = portalColliderSize.x;
+        float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
+        Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+        newPosition.y = contactPoint.y;
+        float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
+            wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
+        newPosition.y = newY;
+        portalSpawn.transform.position = newPosition;
+
+        #endregion
+    }
+    #endregion
+
+    #region Angle = 0 hoặc -180
+
+    private void ChangePortal_0_Nega180(Collider2D wallCollider, float angle, Vector2 portalColliderSize, float wallWidth,
+        Vector2 wallCenter, Vector2 contactNormal, Vector2 contactPoint, GameObject portalSpawn,
+        GameObject portalCurrent)
+    {
+        Spawn_0_Nega180(wallCollider, portalColliderSize, wallCenter, contactNormal, contactPoint, portalSpawn,wallWidth);
+        if (portalCurrent != null)
+        {
+            //đang cùng 1 cột
+            if (Mathf.Abs(PortalManager.Instance._portalSpawn._portalRed.transform.position.y -
+                          PortalManager.Instance._portalSpawn._portalBlue.transform.position.y) < 0.55f)
+            {
+                if (portalCurrent != null)
+                {
+                    float sizePortal = portalCurrent.GetComponent<Collider2D>().bounds.size.x / 2;
+                    float minY = 0;
+                    float maxY = 0;
+                    if (Mathf.Abs(portalSpawn.transform.position.x - portalCurrent.transform.position.x) <
+                        portalCurrent.GetComponent<Collider2D>().bounds.size.x)
+                    {
+                        Debug.Log(123);
+                        if (contactPoint.y > portalCurrent.transform.position.x)
+                        {
+                            minY = portalCurrent.GetComponent<Collider2D>().bounds.max.x + sizePortal;
+                            maxY = wallCollider.bounds.max.x - sizePortal;
+                            Vector2 newPosition1 = (Vector2)portalCurrent.transform.position +
+                                                   new Vector2(0,
+                                                       portalCurrent.GetComponent<Collider2D>().bounds.size.x +
+                                                       .3f);
+                            newPosition1.x = Mathf.Clamp(newPosition1.x, minY, maxY);
+                            portalSpawn.transform.position = newPosition1;
+                        }
+                        else
+                        {
+                            maxY = portalCurrent.GetComponent<Collider2D>().bounds.min.x - sizePortal;
+                            minY = wallCollider.bounds.min.x + sizePortal;
+                            Vector2 newPosition1 = (Vector2)portalCurrent.transform.position -
+                                                   new Vector2(0,
+                                                       portalCurrent.GetComponent<Collider2D>().bounds.size.x +
+                                                       .3f);
+                            newPosition1.x = Mathf.Clamp(newPosition1.x, minY, maxY);
+                            portalSpawn.transform.position = newPosition1;
+                        }
+                    }
+                }
+            }
+        }
+       
+    }
+    private void Spawn_0_Nega180(Collider2D wallCollider, Vector2 portalColliderSize, Vector2 wallCenter, Vector2 contactNormal,
+        Vector2 contactPoint, GameObject portalSpawn, float wallWidth)
+    {
+        Debug.Log(12);
+        #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+        float portalWidth = portalColliderSize.x;
+        float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
+        Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+        newPosition.x = contactPoint.x;
+        float newx = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
+            wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
+        newPosition.x = newx;
+        portalSpawn.transform.position = newPosition;
+
+        #endregion
+    }
+    #endregion
     public void OnCollisionExit2D(Collision2D collision)
     {
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("Portal"))
+        {
+            if (_typeBall == TypeBall.Blue)
+            {
+                Debug.Log("Blue");
+            }
+            else
+            {
+                Debug.Log("Red");
+            }
+        }
     }
 
     public void OnTriggerExit2D(Collider2D other)
