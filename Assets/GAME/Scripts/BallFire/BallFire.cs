13c4d40914325089637551b54e526906e062dfa9
Ring <85309246+Pinochio195@users.noreply.github.com>
2023-06-14 11:11:34 +0700

    Update BallFire.cs

--- Assets/GAME/Scripts/BallFire/BallFire.cs
+++ Assets/GAME/Scripts/BallFire/BallFire.cs
@@ -13,6 +13,7 @@ public class BallFire : MonoBehaviour, ICollidable
     }
 
     public TypeBall _typeBall;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,73 +57,97 @@ public class BallFire : MonoBehaviour, ICollidable
         //Spawn cổng
         if (_typeBall == TypeBall.Blue)
         {
-            if (PortalManager.Instance._portalSpawn._portalBlue!= null)
+            if (PortalManager.Instance._portalSpawn._portalBlue != null)
             {
                 LeanPool.Despawn(PortalManager.Instance._portalSpawn._portalBlue);
             }
+
             PortalManager.Instance._portalSpawn._portalBlue = LeanPool.Spawn(
                 PortalManager.Instance._portalSpawn._prefabsPortalBlue, collision.transform.position,
                 Quaternion.Euler(0, 0, angle));
             //lấy collider của cổng
             Collider2D portalCollider = PortalManager.Instance._portalSpawn._portalBlue.GetComponent<Collider2D>();
-
-            #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
-
-            /*Vector2 portalColliderSize = portalCollider.bounds.size;
             Vector2 wallCenter = wallCollider.bounds.center;
-            float wallWidth = wallCollider.bounds.size.x;
-            float portalWidth = portalColliderSize.x;
-            float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
-            Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-            newPosition.y = contactPoint.y;
-            float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
-                wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
-            newPosition.y = newY;
-            PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;*/
-
-            #endregion
-            #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
-
             Vector2 portalColliderSize = portalCollider.bounds.size;
-            Vector2 wallCenter = wallCollider.bounds.center;
-            float wallHeight = wallCollider.bounds.size.y;
-            float portalHeight = portalColliderSize.y;
-            float distanceFromWallCenter = (wallHeight + portalHeight) * 0.5f;
-            Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-            newPosition.x = contactPoint.x;
-            float newX = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
-                wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
-            newPosition.x = newX;
-            PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;
+            float wallWidth = wallCollider.bounds.size.x;
+            if (angle == 90 || angle == -90)
+            {
+                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
 
-            #endregion
+                float portalWidth = portalColliderSize.x;
+                float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
+                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+                newPosition.y = contactPoint.y;
+                float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
+                    wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
+                newPosition.y = newY;
+                PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;
 
+                #endregion
+            }
+            else
+            {
+                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+
+                float wallHeight = wallCollider.bounds.size.y;
+                float portalHeight = portalColliderSize.y;
+                float distanceFromWallCenter = (wallHeight + portalHeight) * 0.5f;
+                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+                newPosition.x = contactPoint.x;
+                float newX = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
+                    wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
+                newPosition.x = newX;
+                PortalManager.Instance._portalSpawn._portalBlue.transform.position = newPosition;
+
+                #endregion
+            }
         }
         else if (_typeBall == TypeBall.Red)
         {
-            if (PortalManager.Instance._portalSpawn._portalRed!= null)
+            if (PortalManager.Instance._portalSpawn._portalRed != null)
             {
                 LeanPool.Despawn(PortalManager.Instance._portalSpawn._portalRed);
             }
-            PortalManager.Instance._portalSpawn._portalRed = LeanPool.Spawn(PortalManager.Instance._portalSpawn._prefabsPortalRed, collision.transform.position, Quaternion.Euler(0, 0, angle));
+
+            PortalManager.Instance._portalSpawn._portalRed = LeanPool.Spawn(
+                PortalManager.Instance._portalSpawn._prefabsPortalRed, collision.transform.position,
+                Quaternion.Euler(0, 0, angle));
             //lấy collider của cổng
             Collider2D portalCollider = PortalManager.Instance._portalSpawn._portalRed.GetComponent<Collider2D>();
-
-            #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
-
-            Vector2 portalColliderSize = portalCollider.bounds.size;
             Vector2 wallCenter = wallCollider.bounds.center;
+            Vector2 portalColliderSize = portalCollider.bounds.size;
             float wallWidth = wallCollider.bounds.size.x;
-            float portalWidth = portalColliderSize.x;
-            float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
-            Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
-            newPosition.y = contactPoint.y;
-            float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
-                wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
-            newPosition.y = newY;
-            PortalManager.Instance._portalSpawn._portalRed.transform.position = newPosition;
+            if (angle == 90 || angle == -90)
+            {
+                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
 
-            #endregion
+                float portalWidth = portalColliderSize.x;
+                float distanceFromWallCenter = (wallWidth + portalWidth) * 0.5f;
+                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+                newPosition.y = contactPoint.y;
+                float newY = Mathf.Clamp(newPosition.y, wallCollider.bounds.min.y + portalColliderSize.y * 0.5f,
+                    wallCollider.bounds.max.y - portalColliderSize.y * 0.5f);
+                newPosition.y = newY;
+                PortalManager.Instance._portalSpawn._portalRed.transform.position = newPosition;
+
+                #endregion
+            }
+            else
+            {
+                #region Điều chỉnh vị trí của box collider của portal để dính sát vào box collider của wall
+
+                float wallHeight = wallCollider.bounds.size.y;
+                float portalHeight = portalColliderSize.y;
+                float distanceFromWallCenter = (wallHeight + portalHeight) * 0.5f;
+                Vector2 newPosition = wallCenter + contactNormal * distanceFromWallCenter;
+                newPosition.x = contactPoint.x;
+                float newX = Mathf.Clamp(newPosition.x, wallCollider.bounds.min.x + portalColliderSize.x * 0.5f,
+                    wallCollider.bounds.max.x - portalColliderSize.x * 0.5f);
+                newPosition.x = newX;
+                PortalManager.Instance._portalSpawn._portalRed.transform.position = newPosition;
+
+                #endregion
+            }
         }
     }
 
